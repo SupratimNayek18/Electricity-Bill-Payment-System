5633cde0833516211e4832836354d77ea26327dd
SupratimNayek18 <supratimnayek@gmail.com>
2022-08-08 17:22:11 +0530

    Customer entity added

---
+++ electricity-bill-payment-system/src/test/java/electricitybillpaymentsystem/ElectricityBillPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package electricitybillpaymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityBillPaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
